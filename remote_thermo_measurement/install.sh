4bea1be4c536472a9a019162a4ff798c40bfe32f
Steven Presser <steve@pressers.name>
2016-04-03 20:26:15 +0000

    removed -f flag from unzip so it works.

--- remote_thermo_measurement/install.sh
+++ remote_thermo_measurement/install.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 [ -z "$( which unzip )" ] && ( echo "Please install the unzip utility"; exit 1 )
-cd /tmp
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+ls -a
 wget https://github.com/mhrivnak/radiotherm/archive/master.zip
-unzip -f master.zip
+unzip master.zip
 rm master.zip
 cd radiotherm-master
 python setup.py install
 cd ..
 rm -r radiotherm-master
 pip install -U Adafruit_BBIO requests
+
