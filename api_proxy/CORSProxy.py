4e6abb3a98ec1fb5ae9db788dc00d4b71b638098
spresse1 <steve@pressers.name>
2016-04-14 19:03:57 -0400

    Version 0 of CORSProxy,  simple WSGI application to manage proxying requests to a single host, optionally up/downgrading ssl, adding CORS headers and more.

---
+++ api_proxy/CORSProxy.py
@@ -0,0 +1,180 @@
+#! /usr/bin/env python
+
+banned_headers = [
+	'Connection',
+	'Keep-Alive',
+	'Proxy-Authenticate',
+	'Proxy-Authorization',
+	'TE',
+	'Trailers',
+	'Transfer-Encoding',
+	'Upgrade',
+]
+
+from wsgiproxy.exactproxy import proxy_exact_request
+from copy import deepcopy
+
+class CORSProxy:
+	"""A (relatively) sinple class to perform HTTP request proxying and add CORS
+	headers to the response.
+	
+	A simple way to run this is (adjusting hosts and ports, of course):
+	>>> from wsgiref.simple_server import make_server
+	>>> myproxy = CORSProxy("localhost",8081,allow_from=True)
+	>>> server = make_server('127.0.0.1', 8080, myproxy)
+	>>> server.handle_request()
+	"""
+	def __init__(self, server_name, server_port=0, target_protocol = "",
+		allow_from=False, auth=None, add_headers=None):
+		"""
+		Initializer.
+		
+		server_name: Required.  The name/IP of the server to connect to.
+		server_port: The port to connect to.  If unset, the class will guess 
+			based on whether the target is HTTP (80) or HTTPS (443)
+		target_protocol: Controls whether the proxy will use HTTPS to contact 
+			the target.  If set to HTTP (case insensitive), no encryption will 
+			be used. If set to HTTPS, encryption will be used.  If the empty 
+			string (the default), the proxy will guess, based on the method 
+			used to conenct to the proxy.
+		allow_from: Controls the headers that are added to the response.  If 
+			False, the empty string, or the empty list, no headers will be 
+			added.  If True or "*", headers will be added to allow connections 
+			from any domain.  If a list, headers will be added to allow from the
+			named domains only. Entries must be in the form of a URI, including 
+			protocol, e.g.: "http://my-domain/".  Entries are case sensitive.
+			Note that non-CORS-requests (those that do not include an Origin 
+			header) will not have headers added if using a restricted list.
+			However, this WILL NOT stop the request - if you wish to do so, use
+			an auth function (below).
+			Default: False.
+		auth: A function used to authenticate clients.  Passed one 
+			argument: a copy of the environment (which will include all 
+			headers).  Returns True (authentication succeeded) or any other 
+			value.  Returning any value except True will result in HTTP 401 
+			Unauthorized being sent and no proxying of requests will be 
+			performed. If a value other than true is returned, it will be
+			stringified and used as the body of the response.
+			See the SimpleSecretAuth class in this module for an example 
+			implementation.
+		add_headers: A list of ("header name", "value") tuples representing 
+			additional headers to add to the response. Note that some header 
+			filtering may occur AFTER these are added. Default: None
+		
+		Please note: The W3C includes the following statement in their CORS
+			recommendations, with respect to the Access-Control-Allow-Origin
+			header: "In practice the origin-list-or-null production is more 
+			constrained. Rather than allowing a space-separated list of origins,
+			it is either a single origin or the string "null".".  In order to 
+			account for this, this proxy will inspect the origin of the request 
+			and if it is in allow_from or allow_from is True or "*", the origin 
+			as listed in the request will be the origin listed in the request.
+			"*" may still be used if the request does not include an origin 
+			header.  This is most commonly the case if not using a 
+			CORS-compliant browser or if the request does not come via AJAX.
+		
+		Please note: The W3C remarks that no authentication data should be sent
+			to a server which returns * in the Access-Control-Allow-Origin 
+			header on a preflight (OPTIONS) request.  In terms of this class,
+			this means you may not receive standard authentication headers under
+			certain circumstances (specific browsers; javascript libraries) if
+			using allow_from="*".
+			In the example authentication class, a non-standard header is used 
+			to work around this problem.
+
+		For more information on the notes, please visit 
+		https://www.w3.org/TR/cors/
+		"""
+		self.server_name=server_name
+		self.server_port=server_port
+		self.target_protocol=target_protocol.lower()
+		self.allow_from=allow_from
+		self.auth=auth
+		
+		#TODO: validate add_headers is the right format
+		
+		self.add_headers=add_headers
+		
+	def __call__(self, environ, start_response):
+		"""
+		Working function of the class.  Implements the WSGI interface to the 
+		application.  That is, this does the work of proxying.
+		"""
+		self.environ = environ
+		self.start_response= start_response
+		
+		# Start off by examining auth
+		if self.auth is not None:
+			authres = self.auth(deepcopy(environ))
+			if authres is not True:
+				start_response("401 Unauthorized", [])
+				return str(authres)
+		
+		# Okay, let's look at protocol.
+		if self.target_protocol != "":
+			if self.target_protocol != "http" and \
+					self.target_protocol != "https":
+				raise ValueError("target_protocol must be http or https"
+					"(case insensitive)")
+			self.environ['wsgi.url_scheme'] = self.target_protocol
+			if self.target_protocol == "https":
+				self.environ['HTTPS']="on"
+			elif 'HTTPS' in self.environ:
+				del self.environ['HTTPS']
+		# If the user didn't set anything, we want the same scheme to be used
+		# But, force it lower case
+		self.environ['wsgi.url_scheme'] = \
+			self.environ['wsgi.url_scheme'].lower()
+		
+		# Now we can pick the appropriate target port
+		if self.server_port != 0:
+			self.environ['SERVER_PORT'] = str(self.server_port)
+		elif self.environ['wsgi.url_scheme']=="http":
+			self.environ['SERVER_PORT'] = "80"
+		elif self.environ['wsgi.url_scheme']=="https":
+			self.environ['SERVER_PORT'] = "443"
+		else:
+			# Fall through and error
+			raise ValueError("WSGI Environ passed a bad url_scheme")
+		
+		self.environ['HTTP_HOST'] = self.server_name + ":" + \
+			self.environ['SERVER_PORT']
+		self.environ['SERVER_NAME'] = self.server_name
+		
+		return proxy_exact_request(self.environ, self.my_start_response)
+	
+	def my_start_response(self, status, headers):
+		"""
+		A wrapper around the default start_response function.  Designed to work 
+		around limitations of various servers and to massage any headers 
+		required in the response.
+		"""
+		# Start by adding user-requested headers
+		if self.add_headers:
+			headers+=self.add_headers
+		
+		# Filter for headers that will make wsgiref barf.
+		# TODO: Is this needed in general?  Should it be run by default with the
+		# exception of servers known to work?
+		if self.environ['SERVER_SOFTWARE'].split()[0] == "WSGIServer/0.1":
+			headers = [ x for x in headers if x[0] not in banned_headers ]
+			
+		if self.allow_from is True or self.allow_from=="*":
+			if 'ORIGIN' in self.environ:
+				headers.append(("Access-Control-Allow-Origin", 
+					self.environ['ORIGIN']))
+			else:
+				headers.append(("Access-Control-Allow-Origin", "*"))
+		elif self.allow_from is not False and 'ORIGIN' in self.environ:
+			# Origin header sent, we look for a match
+			if self.environ['ORIGIN'] in self.allow_from:
+				headers.append(("Access-Control-Allow-Origin", 
+					self.environ['ORIGIN']))
+			else:
+				# From a non-allowed origin.  Respond with a header, but 
+				# include only the first allowed entry.
+				headers.append(("Access-Control-Allow-Origin", 
+					self.allow_from[0]))
+		# else add no header
+				
+		return self.start_response(status, headers)
---
+++ api_proxy/test_CORSProxy.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+
+# Unit testing is good for the soul.
+import mock
+import unittest
+from CORSProxy import CORSProxy
+
+class Response:
+	status=""
+	headers=[]
+	environ={}
+	
+class test_CORSProxy(unittest.TestCase):
+	def simulate_called(self, environ, start_response):
+		self.response.environ = environ
+		start_response(self.status, self.headers)
+		return mock.DEFAULT
+	
+	def simulate_start_response(self, status, headers):
+		self.response.status = status
+		self.response.headers = headers
+	
+	def setUp(self):
+		self.environ = None
+		self.headers = []
+		self.status = "200 OK"
+		self.response = Response()
+		self.mf = mock.patch('CORSProxy.proxy_exact_request').start()
+		self.mf.return_value="Success!"
+		self.mf.side_effect = self.simulate_called
+		self.cp = CORSProxy('localhost')
+	
+	def get_default_environ(self):
+		return {
+			"GATEWAY_INTERFACE": 'CGI/1.1',
+			"HTTP_ACCEPT": '*/*',
+			"HTTP_HOST": 'localhost:80',
+			"HTTP_USER_AGENT": 'curl/7.47.0',
+			"PATH_INFO": '/',
+			"QUERY_STRING": '',
+			"REMOTE_ADDR": '127.0.0.1',
+			"REMOTE_HOST": 'localhost',
+			"REQUEST_METHOD": 'GET',
+			"SCRIPT_NAME": '',
+			"SERVER_NAME": 'localhost',
+			"SERVER_PORT": "80",
+			"SERVER_PROTOCOL": 'HTTP/1.1',
+			"SERVER_SOFTWARE": 'WSGIServer/0.1 Python/2.7.11+',
+			"wsgi.multiprocess": False,
+			"wsgi.multithread": True,
+			"wsgi.run_once": False,
+			"wsgi.url_scheme": 'http',
+			"wsgi.version": (1, 0),
+		}
+	
+	def test_basic(self):
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEquals(res, "Success!")
+		self.assertEquals(self.response.environ['wsgi.url_scheme'], "http")
+		self.assertEquals(self.response.status, "200 OK")
+	
+	def test_auth_true(self):
+		self.cp = CORSProxy('localhost', auth=lambda x: True)
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEqual(self.response.status, "200 OK")
+		
+	def test_auth_false(self):
+		self.cp = CORSProxy('localhost', auth=lambda x: False)
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEqual(self.response.status, "401 Unauthorized")
+		
+	def test_auth_message(self):
+		self.cp = CORSProxy('localhost', 
+			auth=lambda x: "My error message")
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEqual(self.response.status, "401 Unauthorized")
+		self.assertEqual(res, "My error message")
+	
+	def test_keep_https(self):
+		env = self.get_default_environ()
+		env['wsgi.url_scheme'] = "https"
+		env['HTTPS'] = "on"
+		res = self.cp(env, self.simulate_start_response)
+		self.assertEquals(res, "Success!")
+		self.assertEquals(self.response.environ['wsgi.url_scheme'], "https")
+		self.assertEquals(self.response.environ['HTTPS'], "on")
+	
+	def test_keep_http(self):
+		env = self.get_default_environ()
+		res = self.cp(env, self.simulate_start_response)
+		self.assertEquals(res, "Success!")
+		self.assertEquals(self.response.environ['wsgi.url_scheme'], "http")
+		self.assertNotIn('HTTPS', self.response.environ)
+	
+	def test_force_http_downgrade(self):
+		self.cp = CORSProxy('localhost', target_protocol="http")
+		env = self.get_default_environ()
+		env['wsgi.url_scheme'] = "https"
+		env['HTTPS'] = "on"
+		res = self.cp(env, self.simulate_start_response)
+		self.assertEquals(res, "Success!")
+		self.assertEquals(self.response.environ['wsgi.url_scheme'], "http")
+		self.assertEquals(self.response.environ['SERVER_PORT'], "80")
+		self.assertNotIn('HTTPS', self.response.environ)
+	
+	def test_force_https_upgrade(self):
+		self.cp = CORSProxy('localhost', target_protocol="https")
+		env = self.get_default_environ()
+		res = self.cp(env, self.simulate_start_response)
+		self.assertEquals(res, "Success!")
+		self.assertEquals(self.response.environ['wsgi.url_scheme'], "https")
+		self.assertEquals(self.response.environ['SERVER_PORT'], "443")
+		self.assertEquals(self.response.environ['HTTPS'], "on")
+	
+	def test_bad_target_protocol(self):
+		self.cp = CORSProxy('localhost', target_protocol="junkproto")
+		env = self.get_default_environ()
+		with self.assertRaises(ValueError):
+			self.cp(env, self.simulate_start_response)	
+	
+	def test_cant_fall_through_proto(self):
+		self.cp = CORSProxy('localhost', target_protocol="hTTp")
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEquals(self.response.environ['wsgi.url_scheme'], "http")
+	
+	def test_bad_url_scheme_from_server(self):
+		env = self.get_default_environ()
+		env['wsgi.url_scheme'] = "somejunk"
+		with self.assertRaises(ValueError):
+			self.cp(env, self.simulate_start_response)
+	
+	def test_fix_environ_proto_odd_caps(self):
+		env = self.get_default_environ()
+		env['wsgi.url_scheme'] = "hTTp"
+		res = self.cp(env, self.simulate_start_response)
+		self.assertEquals(self.response.environ['wsgi.url_scheme'], "http")
+	
+	def test_fix_user_forced_proto_odd_caps(self):
+		self.cp.target_protocol = "hTTp"
+		with self.assertRaises(ValueError):
+			self.cp(self.get_default_environ(), self.simulate_start_response)
+	
+	def test_pick_port_http(self):
+		self.cp = CORSProxy('localhost')
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEquals(self.cp.environ['SERVER_PORT'], "80")
+		self.assertEquals(self.response.environ['SERVER_PORT'], "80")
+	
+	def test_pick_port_https(self):
+		self.cp = CORSProxy('localhost')
+		env = self.get_default_environ()
+		env['wsgi.url_scheme'] = "https"
+		res = self.cp(env, self.simulate_start_response)
+		self.assertEquals(self.cp.environ['SERVER_PORT'], "443")
+		self.assertEquals(self.response.environ['SERVER_PORT'], "443")
+	
+	def test_update_host_server(self):
+		self.cp = CORSProxy('self.domain')
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEquals(self.response.environ['SERVER_NAME'], "self.domain")
+		self.assertEquals(self.response.environ['HTTP_HOST'], "self.domain:80")
+	
+	def test_keep_odd_port(self):
+		self.cp = CORSProxy('localhost', '17')
+		res = self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEquals(self.response.environ['HTTP_HOST'], "localhost:17")
+		self.assertEquals(self.cp.environ['SERVER_PORT'], "17")
+		self.assertEquals(self.response.environ['SERVER_PORT'], "17")
+	
+	def test_add_headers(self):
+		hdr = [("X-My-Special-Header", "My-Value")]
+		self.cp = CORSProxy('localhost', add_headers=hdr)
+		self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEquals(hdr, self.response.headers)
+	
+	def test_remove_bad_headers(self):
+		hdr = [
+			("Connection", "My-Value"),
+			("Keep-Alive", "False"),
+			("Proxy-Authenticate", "Nope"),
+			("Proxy-Authorization", "Not here"),
+			('TE', "what"),
+			('Trailers', "None"),
+			('Transfer-Encoding', "Nothing"),
+			('Upgrade', "Dont"),
+		]
+		self.cp = CORSProxy('localhost', add_headers=hdr)
+		self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertEquals([], self.response.headers)
+	
+	def test_dont_remove_bad_headers(self):
+		hdr = [
+			("Connection", "My-Value"),
+			("Keep-Alive", "False"),
+			("Proxy-Authenticate", "Nope"),
+			("Proxy-Authorization", "Not here"),
+			('TE', "what"),
+			('Trailers', "None"),
+			('Transfer-Encoding', "Nothing"),
+			('Upgrade', "Dont"),
+		]
+		self.cp = CORSProxy('localhost', add_headers=hdr)
+		env = self.get_default_environ()
+		env['SERVER_SOFTWARE'] = "Not wsgiref"
+		self.cp(env, self.simulate_start_response)
+		self.assertEquals(hdr, self.response.headers)
+	
+	def test_added_ACAO_star_no_origin(self):
+		self.cp = CORSProxy('localhost', allow_from=True)
+		self.cp(self.get_default_environ(), self.simulate_start_response)
+		self.assertIn(("Access-Control-Allow-Origin", "*"), 
+			self.response.headers)
+	
+	def test_added_ACAO_False_origin(self):
+		self.cp = CORSProxy('localhost', allow_from=False)
+		env = self.get_default_environ()
+		env['ORIGIN'] = "my.domain"
+		self.cp(env, self.simulate_start_response)
+		self.assertNotIn(("Access-Control-Allow-Origin", "my.domain"), 
+			self.response.headers)
+	
+	def test_added_ACAO_True_origin(self):
+		self.cp = CORSProxy('localhost', allow_from=True)
+		env = self.get_default_environ()
+		env['ORIGIN'] = "my.domain"
+		self.cp(env, self.simulate_start_response)
+		self.assertIn(("Access-Control-Allow-Origin", "my.domain"), 
+			self.response.headers)
+	
+	def test_added_ACAO_star_origin(self):
+		self.cp = CORSProxy('localhost', allow_from="*")
+		env = self.get_default_environ()
+		env['ORIGIN'] = "my.domain"
+		self.cp(env, self.simulate_start_response)
+		self.assertIn(("Access-Control-Allow-Origin", "my.domain"), 
+			self.response.headers)
+	
+	def test_ACAO_origin_list_match(self):
+		self.cp = CORSProxy('localhost', 
+			allow_from=["google.com", "my.domain", "microsoft.com"])
+		env = self.get_default_environ()
+		env['ORIGIN'] = "my.domain"
+		self.cp(env, self.simulate_start_response)
+		self.assertIn(("Access-Control-Allow-Origin", "my.domain"), 
+			self.response.headers)
+	
+	def test_ACAO_origin_list_no_match(self):
+		self.cp = CORSProxy('localhost', 
+			allow_from=["google.com", "microsoft.com"])
+		env = self.get_default_environ()
+		env['ORIGIN'] = "my.domain"
+		self.cp(env, self.simulate_start_response)
+		self.assertNotIn(("Access-Control-Allow-Origin", "my.domain"), 
+			self.response.headers)
+		self.assertIn(("Access-Control-Allow-Origin", "google.com"), 
+			self.response.headers)
+		
+if __name__ == '__main__':
+    unittest.main()
