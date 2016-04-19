10b9173fc5dab4904e50b32c1c2a0bba6c36b595
spresse1 <steve@pressers.name>
2016-04-18 21:26:00 -0400

    Updated .travis.sh to a) output non-zro exit code on any command error b) not try to upload coverage results to codacy when run locally (or without the appropriate env vars set

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+EXIT_CODE=0
+trap catch_errors ERR;
+function catch_errors() {
+  EXIT_CODE=$?
+  echo "Exited with $EXIT_CODE
+}
+
 cd remote_thermo_measurement
 tox
-if [ -n "${CODACY_PROJECT_TOKEN}"]
+
+if [ -n "${CODACY_PROJECT_TOKEN}" ]
 then
+	echo "Uploading coverage results to codacy..."
 	pip install codacy-coverage
 	coverage xml
         python-codacy-coverage -r coverage.xml
 fi
 cd ..
+
+exit $EXIT_CODE
