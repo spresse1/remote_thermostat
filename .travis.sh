6e2548d322ee36e9c454991c3abfc6c839bcc2d1
spresse1 <steve@pressers.name>
2016-04-19 20:10:01 -0400

    Okay, actually passing the envvar ths time

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 EXIT_CODE=0
-TOX_TESTENV_PASSENV=CODACY_PROJECT_TOKEN
 
 trap catch_errors ERR;
 function catch_errors() {
@@ -11,6 +10,6 @@ function catch_errors() {
 
 cd remote_thermo_measurement
 tox
-tox -e codacy-coverage-upload
+TOX_TESTENV_PASSENV=CODACY_PROJECT_TOKEN tox -e codacy-coverage-upload
 
 exit $EXIT_CODE
