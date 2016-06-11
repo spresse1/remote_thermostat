7ebad99ecb165e38dd59231c9aec056c38530036
spresse1 <steve@pressers.name>
2016-06-11 17:02:44 -0400

    Added getState and getTarget functions plus associated tests.

--- web_interface/js/ThermoComms.js
+++ web_interface/js/ThermoComms.js
@@ -20,6 +20,10 @@ var ThermoComms = function(address, protocol) {
 	this.thermoModel = "/tstat/model";
 	this.thermoProgramHeat = "/tstat/program/heat";
 	this.thermoProgramCool = "/tstat/program/cool";
+	
+	// Alas, we need to keep track of the model.
+	this.model = undefined;
+	this.version = undefined;
 };
 
 ThermoComms.ajaxFailed = function(xhr, status, errorThrown) {
@@ -46,6 +50,12 @@ ThermoComms.ajaxAlways = function(xhr, status) {
  *     See jQuery's ajax.fail() for details.
  */
 ThermoComms.prototype.getModelVersion = function(success_cb, fail_cb) {
+	if (this.model && this.version) {
+		success_cb(this.model, this.verison)
+	}
+	
+	tcommObject = this
+	
 	$.ajax({
 		url: this.protocol + "://" + this.address + this.thermoModel,
 		type: "GET",
@@ -55,6 +65,10 @@ ThermoComms.prototype.getModelVersion = function(success_cb, fail_cb) {
 		model = json.model.split(" ")[0];
 		version = json.model.split(" ")[1];
 		console.log("Recieved result of", model, version);
+		
+		tcommObject.model = model;
+		tcommObject.version = version;
+		
 		success_cb(model, version);
 	})
 	.fail(function (xhr, status, errorThrown) {
@@ -70,4 +84,133 @@ ThermoComms.prototype.getModelVersion = function(success_cb, fail_cb) {
 	})
 }
 
+/**
+ * Fetches/updates the (volatile) state of the thermostat
+ * Returns nothing
+ * Callback prototypes:
+ *   success_cb(structure):
+ *     A structure, built as follows:
+ *       {
+ *         temp: float, // The current temp
+ *         hvac_state: int, // 0=off, 1= heat, 2=cool
+ *         fan_state: int, // Fan mode 0=off, 1=on
+ *         time:
+ *           day: int, // Day of the week
+ *           hour: int, // Hour of the day
+ *           minute: int, // minute of the hour
+ *       }
+ *   fail_cb(xhr, status, errorThrown)
+ *     See jQuery's ajax.fail() for details.
+ */
+ThermoComms.prototype.getState = function(success_cb, fail_cb) {
+	context = this;
+	// We need to know what model, wrap the whole call as the CB to getting that
+	this.getModelVersion(function() {
+		$.ajax({
+			url: context.protocol + "://" + context.address + context.thermoInfo,
+			type: "GET",
+			dataType: "json",
+		})
+		.done(function (json) {
+			console.log("Recieved result of", json);
+		
+			result = {}
+		
+			result.temp = json.temp
+			result.hvac_state = json.ttarget
+			if (context.model == "CT30") {
+				result.fan_state = json.fstate;
+			} else {
+				result.fan_state = ( json.fmode >=1 ) ? 1 : 0
+			}
+			result.time = json.time
+		
+			success_cb(result);
+		})
+		.fail(function (xhr, status, errorThrown) {
+			/* istanbul ignore else */
+			if (fail_cb !== undefined) {
+				fail_cb(xhr, status, errorThrown)
+			} else {
+				ThermoComms.ajaxFailed(xhr, status, errorThrown)
+			}
+		})
+		.always(function (xhr, status) {
+			ThermoComms.ajaxAlways(xhr, status)
+		})
+	}, fail_cb);
+}
 
+/**
+ * Fetches/updates the (volatile) state of the thermostat
+ * Returns nothing
+ * Callback prototypes:
+ *   success_cb(structure):
+ *     A structure, built as follows:
+ *       {
+ *         temp: float, // The target termperature fo rhte thermostat
+ *         hvac_mode: str, // One of {off, auto, heat, cool}. Operating mode
+ *         fan_mode: str, // 0 = Auto, 1= (always) on
+ *         program: {
+ *           mode: int, // 0= A, 1= B, 2=vacation, 3=Holiday
+ *           override: int, // Whether or not he target is an
+ *                           // override of the program 0= no, 1 = yes
+ *           hold: bool, // Whether the thermostat will hold this temperature
+ *                       // 0 = off, 1 = on
+ *       }
+ *   fail_cb(xhr, status, errorThrown)
+ *     See jQuery's ajax.fail() for details.
+ */
+ThermoComms.prototype.getTarget = function(success_cb, fail_cb) {
+	context = this;
+	// We need to know what model, wrap the whole call as the CB to getting that
+	this.getModelVersion(function() {
+	 	$.ajax({
+			url: context.protocol + "://" + context.address + context.thermoInfo,
+			type: "GET",
+			dataType: "json",
+		})
+		.done(function (json) {
+			console.log("Recieved result of", json);
+		
+			result = {}
+		
+			// Figure out what temperature to read
+			if (json.t_heat) {
+				result.temp = json.t_heat;
+			} else if (json.t_cool) {
+				result.temp = json.t_cool;
+			} else if (json.it_heat) {
+				result.temp = json.it_heat;
+			} else if (json.it_cool) {
+				result.temp = json.it_cool;
+			} else if (json.a_heat) {
+				result.temp = json.a_heat;
+			} else if (json.a_cool) {
+				result.temp = json.a_cool;
+			} else {
+				result.temp = -1
+			}
+			result.hvac_mode = json.tmode;
+			result.fan_mode = ( json.fmode <=1 ) ? 0 : 1 ;
+		
+			result.program = {}
+			result.program.mode = json.program_mode
+			result.program.override = json.override
+			result.program.hold = json.hold
+			
+			success_cb(result);
+		})
+		.fail(function (xhr, status, errorThrown) {
+			/* istanbul ignore else */
+			if (fail_cb !== undefined) {
+				fail_cb(xhr, status, errorThrown);
+			} else {
+				ThermoComms.ajaxFailed(xhr, status, errorThrown);
+			}
+		})
+		.always(function (xhr, status) {
+			ThermoComms.ajaxAlways(xhr, status);
+		})
+	}, fail_cb);
+}
--- web_interface/tests/tests-ThermoComms.js
+++ web_interface/tests/tests-ThermoComms.js
@@ -12,55 +12,411 @@ QUnit.module("Thermostat Communication (ThermoComms)", function(hooks) {
 		console.log("running afterEach");
 		this.server.restore();
 	});
-
-	QUnit.test('get model',function(test) {
-		var done = test.async();
-		this.tcomms = new ThermoComms("localhost");
-
-		var callback = function(name, version) {
-			console.log("Given result of", name, version)
-			test.ok(name==="CT80", "Model name is correct");
-			test.ok(version==="V2.14T", "Version ID is correct");
-			done();
-		};
 	
-		this.tcomms.getModelVersion(callback);
-		this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
-			'{"model":"CT80 V2.14T"}');
+	QUnit.module("ThermoComs.constructor", function(hooks) {	
+		QUnit.test('default address in constructor',function(test) {
+			this.tcomms = new ThermoComms();
+		
+			test.ok(this.tcomms.address==="localhost");
+		});
+
+		QUnit.test('Specify protocol',function(test) {
+			this.tcomms = new ThermoComms("localhost","foobar");
+		
+			test.ok(this.tcomms.protocol==="foobar");
+		});
 	});
 
-	QUnit.test('Test AJAX failure code',function(test) {
-		var done = test.async();
-		this.tcomms = new ThermoComms("localhost")
-		
-		var cb_failure = function(xhr, status, errorThrown) {
-			console.log("In failure branch.");
-			ThermoComms.ajaxFailed(xhr, status, errorThrown);
-			test.ok(true, "Called into failure code");
-			done();
-		}
+	QUnit.module("ThermoComs.getModelVersion", function(hooks) {
+		QUnit.test('get model',function(test) {
+			var done = test.async();
+			this.tcomms = new ThermoComms("localhost");
+			testOb = this
+
+			var callback = function(name, version) {
+				console.log("Given result of", name, version)
+				test.ok(name==="CT80", "Model name is correct");
+				test.ok(version==="V2.14T", "Version ID is correct");
+				test.ok(testOb.tcomms.model=="CT80", "Object has model set");
+				test.ok(
+					testOb.tcomms.version=="V2.14T",
+					"Object has version set");
+				done();
+			};
 	
-		var cb_success = function(name) {
-			console.log("In success mode");
-			test.ok(false, "Called into failure code");
-			done();
-		}
+			this.tcomms.getModelVersion(callback);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+		});
+
+		QUnit.test('Test AJAX failure code',function(test) {
+			var done = test.async();
+			this.tcomms = new ThermoComms("localhost")
+		
+			var cb_failure = function(xhr, status, errorThrown) {
+				console.log("In failure branch.");
+				ThermoComms.ajaxFailed(xhr, status, errorThrown);
+				test.ok(true, "Called into failure code");
+				done();
+			}
 	
-		console.log("About to call into function under test");
-		this.tcomms.getModelVersion(cb_success, cb_failure);
-		this.server.requests[0].respond(404, {}, "");
+			var cb_success = function(name) {
+				console.log("In success mode");
+				test.ok(false, "Called into failure code");
+				done();
+			}
+	
+			console.log("About to call into function under test");
+			this.tcomms.getModelVersion(cb_success, cb_failure);
+			this.server.requests[0].respond(404, {}, "");
 
+		});
 	});
-
-	QUnit.test('default address in constructor',function(test) {
-		this.tcomms = new ThermoComms();
+	
+	QUnit.module("ThermoComms.getState", function(hooks) {
+		hooks.beforeEach( function() {
+			console.log("Running beforeEach in getState");
+			this.server = sinon.fakeServer.create();
+			this.tcomms = new ThermoComms("localhost");
+			this.tcomms.model="CT80";
+			this.tcomms.version="3.15";
+		});
 		
-		test.ok(this.tcomms.address==="localhost");
+		QUnit.test("Basic GetState, model unset", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 45.6, "Temperature set correctly");
+				test.equal(json.hvac_state, 0, "hvac_state set correctly");
+				test.equal(json.fan_state, 0, "fan_state set correctly");
+				test.deepEqual(json.time, {"day": 0, "hour": 1, "minutes": 2 },
+					"time set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getState(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{\
+					"temp": 45.6, \
+					"ttarget": 0, \
+					"fmode": 0, \
+					"time": {"day": 0, "hour": 1, "minutes": 2} \
+				}');
+		});
+		
+		QUnit.test("getState fan on", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 45.6, "Temperature set correctly");
+				test.equal(json.hvac_state, 0, "hvac_state set correctly");
+				test.equal(json.fan_state, 1, "fan_state set correctly");
+				test.deepEqual(json.time, {"day": 0, "hour": 1, "minutes": 2 },
+					"time set correctly");
+				done();
+			}
+
+			this.tcomms.getState(cb_success);
+			this.server.requests[0].respond(
+				200, { "Content-Type": "text/plain" }, 
+				'{"temp": 45.6, "ttarget": 0, "fmode": 2, \
+				"time": {"day": 0, "hour": 1, "minutes": 2} }');
+		});
+		
+		QUnit.test("getState CT30", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 45.6, "Temperature set correctly");
+				test.equal(json.hvac_state, 0, "hvac_state set correctly");
+				test.equal(json.fan_state, 0, "fan_state set correctly");
+				test.deepEqual(json.time, {"day": 0, "hour": 1, "minutes": 2 },
+					"time set correctly");
+				done();
+			}
+
+			this.tcomms.model = "CT30";
+			this.tcomms.getState(cb_success);
+			this.server.requests[0].respond(
+				200, { "Content-Type": "text/plain" }, 
+				'{"temp": 45.6, "ttarget": 0, "fstate": 0, \
+				"time": {"day": 0, "hour": 1, "minutes": 2} }');
+		});
+		
+		QUnit.test('Test AJAX failure code',function(test) {
+			var done = test.async();
+		
+			var cb_failure = function(xhr, status, errorThrown) {
+				console.log("In failure branch.");
+				ThermoComms.ajaxFailed(xhr, status, errorThrown);
+				test.ok(true, "Called into failure code");
+				done();
+			}
+	
+			var cb_success = function(name) {
+				console.log("In success mode");
+				test.ok(false, "Called into failure code");
+				done();
+			}
+	
+			console.log("About to call into function under test");
+			this.tcomms.getState(cb_success, cb_failure);
+			this.server.requests[0].respond(404, {}, "");
+
+		});
 	});
-
-	QUnit.test('Specify protocol',function(test) {
-		this.tcomms = new ThermoComms("localhost","foobar");
+	
+	QUnit.module("ThermoComms.getTarget", function(hooks) {
+		hooks.beforeEach( function() {
+			console.log("Running beforeEach in getTarget");
+			this.server = sinon.fakeServer.create();
+			this.tcomms = new ThermoComms("localhost");
+			this.tcomms.model="CT80";
+			this.tcomms.version="3.15";
+		});
 		
-		test.ok(this.tcomms.protocol==="foobar");
+		QUnit.test("Basic getTarget", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 43.5, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 0, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{\
+					"t_heat": 43.5, \
+					"tmode": 0, \
+					"fmode": 0, \
+					"program_mode": 0, \
+					"override": 0, \
+					"hold": 0 \
+				}');
+		});
+		
+		QUnit.test("getTarget fan on", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 43.5, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 1, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{"t_heat": 43.5, "tmode": 0, "fmode": 2, "program_mode": 0, \
+					"override": 0, "hold": 0 }');
+		});
+		
+		QUnit.test("getTarget t_cool", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 43.5, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 1, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{"t_cool": 43.5, "tmode": 0, "fmode": 2, "program_mode": 0, \
+					"override": 0, "hold": 0 }');
+		});
+		
+		QUnit.test("getTarget it_heat", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 43.5, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 1, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{"it_heat": 43.5, "tmode": 0, "fmode": 2, "program_mode": 0, \
+					"override": 0, "hold": 0 }');
+		});
+		
+		QUnit.test("getTarget it_cool", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 43.5, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 1, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{"it_cool": 43.5, "tmode": 0, "fmode": 2, "program_mode": 0, \
+					"override": 0, "hold": 0 }');
+		});
+		
+		QUnit.test("getTarget a_heat", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 43.5, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 1, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{"a_heat": 43.5, "tmode": 0, "fmode": 2, "program_mode": 0, \
+					"override": 0, "hold": 0 }');
+		});
+		
+		QUnit.test("getTarget a_cool", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, 43.5, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 1, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{"a_cool": 43.5, "tmode": 0, "fmode": 2, "program_mode": 0, \
+					"override": 0, "hold": 0 }');
+		});
+		
+		QUnit.test("getTarget no temp", function(test) {
+			var done = test.async();
+			var cb_success = function(json) {
+				console.log("In success mode");
+				test.ok(true, "Call is success");
+				test.equal(json.temp, -1, "Temperature set correctly");
+				test.equal(json.hvac_mode, 0, "hvac_state set correctly");
+				test.equal(json.fan_mode, 0, "fan_state set correctly");
+				test.equal(json.program.mode, 0, "program set correctly");
+				test.equal(json.program.override, 0, 
+					"program override set correctly");
+				test.equal(json.program.hold, 0, "Program hold set correctly");
+				done();
+			}
+			
+			this.tcomms.model=undefined;
+			this.tcomms.version=undefined;
+
+			this.tcomms.getTarget(cb_success);
+			this.server.requests[0].respond(200, { "Content-Type": "text/plain" }, 
+				'{"model":"CT80 V2.14T"}');
+			this.server.requests[1].respond(200, { "Content-Type": "text/plain" }, 
+				'{"tmode": 0, "fmode": 0, "program_mode": 0, \
+					"override": 0, "hold": 0 }');
+		});
+		
+		QUnit.test('Test AJAX failure code',function(test) {
+			var done = test.async();
+		
+			var cb_failure = function(xhr, status, errorThrown) {
+				console.log("In failure branch.");
+				ThermoComms.ajaxFailed(xhr, status, errorThrown);
+				test.ok(true, "Called into failure code");
+				done();
+			}
+	
+			var cb_success = function(name) {
+				console.log("In success mode");
+				test.ok(false, "Called into failure code");
+				done();
+			}
+	
+			console.log("About to call into function under test");
+			this.tcomms.getTarget(cb_success, cb_failure);
+			this.server.requests[0].respond(404, {}, "");
+
+		});
 	});
 });
